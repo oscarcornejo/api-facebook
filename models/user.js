bfc01fb415a703152e258f2f27c643a2032570cb
oscarcornejo <oscarcornejo10@gmail.com>
2017-09-19 17:48:39 -0300

    Config App

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var findOrCreate = require("mongoose-findorcreate");
 
 var Schema = mongoose.Schema;
-mongoose.connect("mongodb://localhost/taller_facebook");
+mongoose.connect("mongodb://heroku_4zr5vr3g:dolarboy1@ds139964.mlab.com:39964/heroku_4zr5vr3g");
 
 var userSchema = new Schema({
 	name: String,
