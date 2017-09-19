d12b224cfdf37ec95069124117446513fd60848e
oscarcornejo <oscarcornejo10@gmail.com>
2017-09-19 00:56:25 -0300

    Config App

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var findOrCreate = require("mongoose-findorcreate");
 
 var Schema = mongoose.Schema;
-mongoose.connect("mongodb://app-facebook-api.herokuapp.com/taller_facebook");
+mongoose.connect("mongodb://buscar-amigos.com/taller_facebook");
 
 var userSchema = new Schema({
 	name: String,
