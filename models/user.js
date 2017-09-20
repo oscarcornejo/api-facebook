b166d1fe913339c364e79767bd716187f84c16b5
oscarcornejo <oscarcornejo10@gmail.com>
2017-09-19 21:46:03 -0300

    Config App

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var findOrCreate = require("mongoose-findorcreate");
 var Schema = mongoose.Schema;
 
 var mongoURI = "mongodb://localhost/taller_facebook";
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || mongoURI);
 
 process.env.MONGOLAB_URI
 var userSchema = new Schema({
